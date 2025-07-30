c580fcda0b7ad4c37057990bb9399f35ac28631e
Sophia Gomez <114206006+piagooo@users.noreply.github.com>
2025-07-30 10:20:53 -0600

    Add files via upload

---
+++ js/image-expand.js
@@ -0,0 +1,12 @@
+
+
+function expandImage(){
+    document.getElementById("myModal").style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+var span = document.getElementsByClassName("close")[0];
+function closeImage() { 
+    document.getElementById("myModal").style.display = "none";
+}
+
